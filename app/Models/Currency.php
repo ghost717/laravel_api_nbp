7a39b87524afa9f904f39f7c051df934a13ff466
ghost717 <zawadzki7717@interia.pl>
2023-05-08 09:01:24 +0200

    model i migracja dla encji Currency

---
+++ app/Models/Currency.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Currency extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'currency_code',
+        'exchange_rate',
+    ];
+}
